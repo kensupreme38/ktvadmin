31a5808b21f80767afb66ff8af57a14d9c8524c1
Supreme Ken <kensupreme38@gmail.com>
2025-09-20 15:59:19 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,6 +1,6 @@
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/componentsui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
