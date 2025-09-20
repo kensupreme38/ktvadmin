f66b1d9c15610d72549417301f60e7fe472d5d38
Supreme Ken <kensupreme38@gmail.com>
2025-09-20 10:18:06 +0000

    đổi font chữ đi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Montserrat', 'sans-serif'],
+        headline: ['Bebas Neue', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
